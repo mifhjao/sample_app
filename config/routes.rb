df7ffbb42c64e519242b2d1e00d0570b9b2bb287
EC2 Default User <ec2-user@ip-172-31-20-202.ap-northeast-1.compute.internal>
2023-03-23 09:11:14 +0000

    [add]バリエーションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/top'=>'homes#top'
   get 'lists/:id'=> 'lists#show',as: 'list'
   patch 'lists/:id'=>'lists#update'
+  delete 'lists/:id' => 'lists#destory', as:'destory_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
